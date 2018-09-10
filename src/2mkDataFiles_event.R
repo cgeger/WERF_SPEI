18007e9297d11852895ef485281f263643d9da5c
CGEGER <cgeger@gmail.com>
2018-09-10 11:57:57 -0400

    Precip/event merge

---
+++ src/2mkDataFiles_event.R
@@ -0,0 +1,148 @@
+## CLEAN FILES FOR STUDY : event and precip
+## referencing original versions from master set "data/master.RData"
+
+#load packages
+install.packages("lubridate")
+library(lubridate)
+library(dplyr)
+
+##### import raw df #####
+#set working address to the VPN-connected drive where the data is stored
+setwd("H:\WERFproject")
+
+#event is "EVENT" table in ACCESS
+m9 <- readRDS("H:/WERFproject/data/master.RData")[[9]]
+event <- m9
+#precip is "PRECIPITATION" table in ACCESS
+m27 <- readRDS("H:/WERFproject/data/master.RData")[[27]]
+precip <- m27
+
+##### eliminate duplicate rows #####
+#find primary keys (primary keys have zero duplicates)
+sum(duplicated(event$`Record No`)) #zero duplicates on the primary key
+sum(duplicated(precip$`Record Number`)) #zero duplicates on the primary key
+#find duplicate entries (excluding the primary key)
+sum(duplicated.data.frame(event[,-which(names(event) == "Record No")]))
+sum(duplicated.data.frame(precip[,-which(names(precip) == "Record Number")])) #140 duplicates
+
+names(event) #the event table uses a two-factor primary key: NSWID + Storm Event
+
+#make a single primary key by combining NSWID and storm event numbers
+event <- event %>% mutate(pk = paste(event$NSWID, event$`Storm Event`, sep = "_"))
+#make a single primary key for precip by combining Test Site ID and storm event numbers
+precip <- precip %>% mutate(pk = paste(precip$`Test Site ID`, precip$`Storm Event`, sep = "_"))
+
+#make a list of 2-factor primary keys that have duplicates (event table)
+dups <- event %>% 
+  group_by(pk) %>%
+  summarize(n = n()) %>%
+  filter(n > 1) %>%
+  select(pk) 
+
+#find the duplicates in the event table
+k <- which(event$pk %in% dups$pk)
+
+#find the sites that have duplicates <-- there are only 5 sites with duplicates
+event[k,] %>% arrange(pk) %>% group_by(`Site Name`) %>% summarize(n = n())
+
+event[k,] %>% filter(`Site Name` == "1324 East 76th Street")
+event[event$NSWID == "836379890",] #drop record number 22979
+precip[precip$`Test Site ID` == "836379890",]
+precip[precip$pk == "836379890_89",] #two events on the same date, but not on one from event table
+
+event[k,] %>% filter(`Site Name` == "21st and Iris Rain Garden") #drop record number 22979
+event[event$NSWID == "1708200120",] #drop record number 19502, 19503
+precip[precip$pk == "1708200120_41",] #from may 7th 2014
+precip[precip$pk == "1708200120_42",] #from may 8th 2014
+
+#there are more than 100 duplicates for each of these sites, labelling is not clear, so they were eliminated
+event[k,] %>% filter(`Site Name` == "Elm Drive") %>% arrange(pk) #this one reports end dates that are months/years befor start dates
+event[k,] %>% filter(`Site Name` == "Lakeview 2") %>% arrange(pk) #this one reports at least 2 different sensors, but no way to connect that to event date reliably
+precip[precip$`Test Site Name` == "Lakeview 2",] %>% arrange(pk)
+event[k,] %>% filter(`Site Name` == "Lakeview 4") %>% arrange(pk) #this one reports at least 2 different sensors, but no way to connect that to event date reliably
+
+#_drop duplicates from event______________________________________________________
+#drops
+event <- event[!(event$`Record No` %in% c(22979,19502, 19503)), ]
+event <- event[!(event$`Site Name` %in% c("Elm Drive", "Lakeview 2", "Lakeview 4")), ]
+###NO MORE EVENT DUPLICATES (supposedly)
+
+#find duplicate entries (excluding the primary key)
+##sum(duplicated.data.frame(event[,-which(names(event) == "Record No")]))
+##sum(duplicated.data.frame(precip[,-which(names(precip) == "Record Number")])) #141 duplicates
+##precip[k,]
+##k <- which(duplicated.data.frame(precip[,-which(names(precip) == "Record Number")]))
+##precip <- precip[-k,]
+
+##### clean event data frame #####
+#rename variables to uniform names
+names(event) 
+names <- c("SITEID", "EVENTNO", "SITENAME", "EVSTARTDATE", "EVSTARTTIME", "EVTYPE",      
+           "EVANTEDRY", "EVDESCRIBEANTE", "EVQAQC", "EVComment", "EVRecordNO", "EVpk")
+names(event) <- names
+event$SITEID <- trimws(as.character(event$SITEID))
+event$EVENTNO <- as.integer(event$EVENTNO)
+event$EVSTARTDATE <- ymd(substr(event$EVSTARTDATE, start = 1, stop = 10))
+head(event)
+
+
+
+names(precip) 
+names <- c("SITEID","SITENAME", "MSID", "EVENTNO", "PSTARTDATE", "PSTARTTIME", "PENDDATE","PENDTIME",
+           "PDEPTH","PDEPTHU", "P1HRPK", "P1HRPKU", "PComment", "PRecordNO", "PScrn", "Ppk")
+names(precip) <- names
+precip$SITEID <- trimws(as.character(precip$SITEID))
+precip$EVENTNO <- as.integer(precip$EVENTNO)
+precip$PSTARTDATE <- ymd(substr(precip$PSTARTDATE, start = 1, stop = 10))
+head(precip)
+
+#merge event and precip into one table based on pk
+a <- merge(event, precip, all = F, by.x = "EVpk", by.y = "Ppk")
+head(a)
+
+# fix duplicate column names
+names(a)
+k <- which(names(a) %in% c("SITENAME.y", "SITEID.y", "EVENTNO.y"))
+a <- a[,-k]
+k <- which(names(a) %in% c("SITENAME.x", "SITEID.x", "EVENTNO.x"))
+names(a)[k] <- c("SITENAME", "SITEID", "EVENTNO")
+
+names(a)
+#trim character identifiers
+a$MSID <- as.character(a$MSID)
+
+#fix dates
+#check which are more than 48 hours apart
+k <- which(abs(ymd(a$EVSTARTDATE) - ymd(a$PSTARTDATE)) > 2)
+a[k,c("EVSTARTDATE", "PSTARTDATE")]
+a <- a[-k,] #remove
+
+#eliminate non-flow data and events with no depth recorded
+rmV <- which(a$EVTYPE != "Runoff" & is.na(a$PDEPTH))
+a <- a[-rmV,]
+a$EVTYPE <- droplevels(a$EVTYPE)
+rmV <- which(a$PDEPTH < 0 | is.na(a$PDEPTH))
+a <- a[-rmV,]
+
+#correct factors
+a$PScrn <- factor(tolower(a$PScrn))
+
+#convert units
+k <- which(a$PDEPTHU == "in")
+a$PDEPTH[k] <- a$PDEPTH[k] * 2.54
+a$PDEPTHU[k] <- "cm"
+a$PDEPTHU <- droplevels(a$PDEPTHU)
+
+#fix dates
+a$PENDDATE <- parse_date_time(a$PENDDATE, orders = c("m/d/y", "m-d-y", "d-m-y", "d-m-y", "m/d/y H:M:S")) 
+
+head(a)
+
+ev.precip <- a
+
+head(ev.precip)
+
+##### save event table #####
+write.csv(ev.precip,"data/eventprecip.csv") #saved as .csv file in data folder
+saveRDS(ev.precip, "data/eventprecip.Rdata")
+ev.precip <- readRDS("data/eventprecip.Rdata")
